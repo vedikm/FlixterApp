9eed1b28b672dfc94513252853f2dbd07a7b3333
vedik <vedik2.m@gmail.com>
2021-02-27 21:50:43 -0800

    "Finished Flixter Part 2"

---
+++ Flixter/MovieDetailsViewController.swift
@@ -0,0 +1,51 @@
+//
+//  MovieDetailsViewController.swift
+//  Flixter
+//
+//  Created by Vedik Munukutla on 2/26/21.
+//
+
+import UIKit
+import Alamofire
+class MovieDetailsViewController: UIViewController {
+    
+    var movie: [String:Any]!
+    
+    
+    @IBOutlet weak var backdropView: UIImageView!
+    @IBOutlet weak var posterView: UIImageView!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        titleLabel.text = movie["title"] as? String
+        titleLabel.sizeToFit()
+        synopsisLabel.text = movie["overview"] as? String
+        synopsisLabel.sizeToFit()
+        // Do any additional setup after loading the view.
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl = URL(string: baseUrl + posterPath)
+        posterView.af_setImage(withURL: posterUrl!)
+        
+        
+        let backdropPath = movie["backdrop_path"] as! String
+        
+        
+        let backdropUrl = URL(string: "https://image.tmdb.org/t/p/w780" + posterPath)
+        backdropView.af_setImage(withURL: backdropUrl!)
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Flixter/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flixter
+//
+//  Created by Vedik Munukutla on 2/27/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+
+    @IBOutlet weak var posterView: UIImageView!
+}
